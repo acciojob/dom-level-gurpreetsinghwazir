d3c66a5f346eb14c98a6625539d8116d1b33b759
Dragon_Fly <143097451+gurpreetsinghwazir@users.noreply.github.com>
2024-05-19 01:43:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+let selectAll = document.getElementsByTagName("li");
+let level = document.getElementById("level");
+let newLevel = document.getElementById('level').textContent;
+
+let count = 0;
+for (let i = 0; i < selectAll.length; i++) {
+    
+    if (selectAll[i].textContent == newLevel) {
+    
+        break;
+    }
+    	count++;
+}
+
+console.log(count);
